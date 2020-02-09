bcf1a34c362fdb20c10ffb4b82a6585253e5e574
uto-usui <nattyanoranged@gmail.com>
2020-02-09 13:28:19 +0900

    ✨ Add TitleLevel2 ✨

---
+++ components/TitleLevel2.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="title-level2" v-html="text" />
+</template>
+<script>
+export default {
+  name: 'TitleLevel2',
+
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.title-level2 {
+  //
+}
+</style>
