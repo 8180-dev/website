4d49ae8bad452e8f01bb9670dec890e21ec1328a
uto-usui <nattyanoranged@gmail.com>
2020-02-10 21:36:58 +0900

    🔧 Update LazyLoad setting 🔧

--- plugins/vue-lazyload.client.js
+++ plugins/vue-lazyload.client.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
 
 Vue.use(VueLazyload, {
-  preLoad: 1.5,
+  preLoad: 1,
   attempt: 1,
   observer: true,
   observerOptions: {
-    rootMargin: '800px',
-    threshold: 0.95,
+    rootMargin: '200%',
+    threshold: 0.01,
   },
 })
